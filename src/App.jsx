90d01d7da29b9febf3682dca91189ccfa329d8a1
SamarHayatOfficial <samarhayat.dev@gmail.com>
2024-08-28 22:18:33 +0500

    1:22:35

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Model from './components/Model';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
-
-import * as Sentry from '@sentry/react';
-
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Highlights from "./components/Highlights";
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
-      <Model />
-      <Features />
-      <HowItWorks />
-      <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default Sentry.withProfiler(App);
+export default App;
